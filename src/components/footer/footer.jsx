03d3a63eb82c843278a62860cc24d18f4ee2a8ae
Biloliddin_dev <bilol9876@gmail.com>
2023-03-08 23:11:43 +0500

    NEW RESPONSIV

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 import { useDataFetch } from "../hooks/getData.js";
 import "../../App.css";
-
 import cl from "./footer.module.scss";
-import { Link } from "react-router-dom";
 export const Footer = () => {
   let { data } = useDataFetch(["information"], "/information");
   const infoData = data?.data[0];
-
   return (
     <div>
       <footer className={cl.footer}>
-        <div className="container">
+        <div className={cl.container}>
           <div className={cl.footer__wrapper}>
             <div className={cl.footer__info}>
-              <a>
+              <a className={cl.footer__folder}>
                 <img
                   src="https://cdn-icons-png.flaticon.com/512/3082/3082383.png"
                   width={30}
                 />
                 <p>address:{infoData?.address}</p>
               </a>
-              <a>
+              <a
+                href={`https://${infoData?.email}`}
+                className={cl.footer__folder}
+              >
                 <img
                   src="https://cdn0.iconfinder.com/data/icons/apple-apps/100/Apple_Mail-512.png"
                   width={30}
                 />
                 <p>email:{infoData?.email}</p>
               </a>
-              <a src={infoData?.telegram}>
+              <a href={infoData?.telegram} className={cl.footer__folder}>
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/480px-Telegram_logo.svg.png"
                   alt=""
@@ -36,36 +36,33 @@ export const Footer = () => {
                 />
                 <p>telegram:{infoData?.telegram}</p>
               </a>
-              <Link>
+              <a href={infoData?.instagram} className={cl.footer__folder}>
                 <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUW8_2xflThAf1MA2NKFrZ5P6uc6T3yuMh4dfqyPpIDw&s"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"
                   alt=""
                   width={30}
                 />
                 <p>instagram:{infoData?.instagram}</p>
-              </Link>
-              <Link>
+              </a>
+              <a href={`tel:+${infoData?.phone}`} className={cl.footer__folder}>
                 <img
                   src="https://cdn-icons-png.flaticon.com/512/725/725624.png"
                   alt=""
                   width={30}
                 />
                 <p>Phone :{infoData?.phone}</p>
-              </Link>
+              </a>
             </div>
             <div
-              className={cl.footer__map}
+              className={cl.map}
               dangerouslySetInnerHTML={{ __html: infoData?.addressMap }}
             ></div>
           </div>
+          <p className={cl.text}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+          </p>
         </div>
       </footer>
     </div>
   );
 };
-
-//
-//
-//
-//
-//
