6b6917b794c7dc833d6ea095fe7fbdbc09bba53b
Biloliddin_dev <bilol9876@gmail.com>
2023-03-10 21:19:19 +0500

    add home pag

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,11 +1,52 @@
 import React from "react";
-import Comments from "../../components/comments/comments";
-import Scard from "../../components/Scard/homecard";
-import Categ from "../../components/Scateg/categ";
-import c from "./home.module.scss";
-import { useSearchParams } from "react-router-dom";
-import { useGetData } from "../../hooks/useQueryHook";
+import "./home.scss";
+import { Carousel } from "antd";
 
-export default function Home() {
-  return <div className={c.home}></div>;
-}
+const Home = () => {
+  return (
+    <div className="homepage">
+      <div
+        style={{
+          width: "1200px",
+          height: "300px",
+          backgroundColor: "darkblue",
+        }}
+        className="Carusel__wrapper"
+      >
+        <Carousel autoplay dots={true} pauseOnHover dotPosition={"right"}>
+          <div>
+            <h2 style={{ color: "white", lineHeight: "300px" }}>
+              "Shop Savvy: Your One-Stop Online Marketplace"
+            </h2>
+          </div>
+          <div>
+            <h2 style={{ color: "white", lineHeight: "300px" }}>
+              Article on the Benefits of Yoga for Mental Health
+            </h2>
+          </div>
+          <div>
+            <h2 style={{ color: "white", lineHeight: "300px" }}>
+              Creative Writing Piece - A Walk in the Woods:
+            </h2>
+          </div>
+          <div>
+            <h2 style={{ color: "white", lineHeight: "300px" }}>
+              Product Review of a Smartwatch:
+            </h2>
+          </div>
+        </Carousel>
+      </div>
+      <h1 className="homepage-title">Welcome to My Website!</h1>
+      <p className="homepage-description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit
+        amet nibh at odio ultricies blandit. Nullam euismod eros vel libero
+        faucibus, vel bibendum dolor auctor. Proin iaculis, quam at finibus
+        consectetur, purus nulla hendrerit ipsum, sit amet pretium tellus augue
+        id lectus. Nulla facilisi.
+      </p>
+      <button className="homepage-button">Learn More</button>
+    </div>
+  );
+};
+
+export default Home;
