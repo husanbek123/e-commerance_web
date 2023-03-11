c067727cdf164906846282a5c0dc50ab7946e865
aqudratov52@gmail.com <aqudratov52@gmail.com>
2023-03-11 17:13:44 +0500

    AzizFinal

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,26 +1,42 @@
 import React from "react";
 import { useGetData } from "../../hooks/useQueryHook";
 import cl from "./stayle.module.scss";
-import { Link, useLocation, useParams, useSearchParams } from "react-router-dom";
+import {
+  Link,
+  useLocation,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
 
-export const Sidebar = ({ value, categories, lang}) => {
+export const Sidebar = ({ value, categories, lang }) => {
   const { data } = useGetData(["category"], "/category");
   const [param, setParam] = useSearchParams();
 
-  let params = useParams() 
-  let loc = useLocation()
+  let params = useParams();
+  let loc = useLocation();
 
-  let direction = loc.pathname.replace("/categories/", '')
+  let direction = loc.pathname.replace("/categories/", "");
   console.log(loc.pathname, "111111111111111111111");
   return (
-    <div className={[cl.bar, value == true && cl.open].join(" ") }>
+    <div className={[cl.bar, value == true && cl.open].join(" ")}>
       <div className={cl.bar__list}>
-        <Link to={'/products'}  className="link" ><p className={loc.pathname == '/products' && 'activeLink'}>All products</p></Link>
-        {
-          categories?.map(item => (
-            <Link className="link" to={`/categories/${item.id}`}><p className={item.id == direction && "activeLink"}>{item[`name_${lang}`] }</p></Link>
-          ))
-        }
+        <Link to={"/products"} className="link">
+          <p className={loc.pathname == "/products" && "activeLink"}>
+            All products
+          </p>
+        </Link>
+        <Link to={"/categories"} className="link">
+          <p className={loc.pathname == "/categories" && "activeLink"}>
+            All categories
+          </p>
+        </Link>
+        {categories?.map((item) => (
+          <Link className="link" to={`/categories/${item.id}`}>
+            <p className={item.id == direction && "activeLink"}>
+              {item[`name_${lang}`]}
+            </p>
+          </Link>
+        ))}
       </div>
     </div>
   );
